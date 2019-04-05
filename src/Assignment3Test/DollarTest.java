41680a58637472ba238b5640478c94227df64f0e
Jerry Yang <hyan927@aucklanduni.ac.nz>
2019-04-05 15:42:43 +1300

    update library

--- src/Assignment3Test/DollarTest.java
+++ src/Assignment3Test/DollarTest.java
@@ -1,9 +1,7 @@
 package Assignment3Test;
 
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Assert;
+import static org.junit.Assert.*;
 //import org.bson.Document;
 import org.junit.Test;
 
