8708b3ad595fdc3110c01d82a6e724dc5ada7886
Jerry Yang <hyan927@aucklanduni.ac.nz>
2019-04-09 14:18:47 +1200

    try with maven

--- src/com/jy/code/Dollar.java
+++ src/com/jy/code/Dollar.java
@@ -1,4 +1,4 @@
-package Assignment3;
+package com.jy.code;
 
 public class Dollar{
 	private int amount;
--- src/com/jy/test/DollarTest.java
+++ src/com/jy/test/DollarTest.java
@@ -1,11 +1,11 @@
-package Assignment3Test;
+package com.jy.test;
 
 
 import static org.junit.Assert.*;
 //import org.bson.Document;
 import org.junit.Test;
 
-import Assignment3.Dollar;
+import com.jy.code.Dollar;
 //import com.mongodb.MongoClient;
 //import com.mongodb.client.MongoCollection;
 //import com.mongodb.client.MongoDatabase;
@@ -17,7 +17,7 @@ public class DollarTest {
 		// When
 		Dollar result = ten.dividedBy(2);
 		// Then
-		assertEquals(new Dollar(3), result); // You could change 5 to 4 to see the build failure
+		assertEquals(new Dollar(5), result); // You could change 5 to 4 to see the build failure
 	}
 	@Test(expected=ArithmeticException.class)
 	public void testDollarDividedByZero() {
